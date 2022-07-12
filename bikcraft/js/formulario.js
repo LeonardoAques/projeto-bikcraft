a2ee956ec781547762a65f77c1a4594b88a55982
LeonardoAques <aques.leo@gmail.com>
2022-07-12 12:31:26 -0300

    Projeto atualização5 12/07

---
+++ bikcraft/js/formulario.js
@@ -0,0 +1,28 @@
+// Como não tenho uma HOSPEDAGEM, esse JS serve apenas como um exemplo de como ficaria o envio de formulário do site.
+
+const formulario = document.querySelector('form');
+
+function formularioEnviado(resposta) {
+  if(resposta.ok) {
+    formulario.innerHTML = '<p class="font-2-l" style="grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;"><span style="color: #317a00">Mensagem enviada!</span> Em breve entraremos em contato. Geralmente respondemos em 24 horas.</p>'
+  } else {
+    formulario.innerHTML = '<p class="font-2-l" style="grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;"><span style="color:#E00000">Erro no envio</span>, você pode enviar diretamente para o nosso email em: contato@bikcraft.net</p>'
+  }
+}
+
+function enviarFormulario(event) {
+  event.preventDefault();
+  const botao = document.querySelector('form button');
+  botao.disabled = true;
+  botao.innerText = 'Enviando...';
+
+  const data = new FormData(formulario);
+
+  fetch('./enviar.php', {
+    method: 'POST',
+    body: data
+    }).then(formularioEnviado);
+  // 'enviar.php' não existe nos arquivos pois não fiz a compra da Hospedagem. Novamente, esse JS serve como exemplo de como ficaria.
+};
+
+formulario.addEventListener('submit', enviarFormulario);
